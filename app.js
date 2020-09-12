029fade5a0d3aee0cbe4e9241ac1b9a6230897a3
shikto-tech <71165904+shikto-tech@users.noreply.github.com>
2020-09-12 11:43:22 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-var http = require('http');
+const http = require('http');
+const port = process.env.PORT || 3000
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(8080);
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello Whdjdbdjdnrld</h1>');
+});
+
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
